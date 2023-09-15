2a671b18a1a0225c45a398f0543225c07bce20e7
VuongDuyLong <102531616+VuongDuyLong@users.noreply.github.com>
2023-09-15 08:26:02 +0700

    15/9

---
+++ Styles/TextStyle.js
@@ -0,0 +1,87 @@
+const text_styles ={
+    Regular0 :{
+      fontSize  :10,
+      lineHeight:14
+    },
+    Regular1 :{
+      fontSize  :12,
+      lineHeight:16
+    },
+    Regular2 :{
+      fontSize  :14,
+      lineHeight:22
+    },
+    Regular3 :{
+      fontSize  :16,
+      lineHeight:28
+    },
+    Regular4 :{
+      fontSize  :20,
+      lineHeight:28
+    },
+    Regular5 :{
+      fontSize  :24,
+      lineHeight:32
+    },
+    Regular6 :{
+      fontSize  :30,
+      lineHeight:38
+    },
+    Regular7 :{
+      fontSize  :38,
+      lineHeight:48
+    },
+    Regular8 :{
+          fontSize  :46,
+          lineHeight:54
+    },
+    Regular9 :{
+      fontSize  :56,
+      lineHeight:64
+    },
+    Medium0 :{
+      fontSize  :12,
+      lineHeight:20
+    },
+    Medium1 :{
+      fontSize  :14,
+      lineHeight:22
+    },
+    Medium2 :{
+      fontSize  :16,
+      lineHeight:24
+    },
+    Medium3 :{
+      fontSize  :20,
+      lineHeight:28
+    },
+    Medium4 :{
+      fontSize  :20,
+      lineHeight:28
+    }, 
+    Medium5 :{
+      fontSize  :24,
+      lineHeight:32
+    },
+    Medium6 :{
+      fontSize  :30,
+      lineHeight:38
+    }, 
+    Medium7 :{
+      fontSize  :38,
+      lineHeight:48
+    }, 
+    Medium8 :{
+      fontSize  :46,
+      lineHeight:54
+    }, 
+    Medium9 :{
+      fontSize  :56,
+      lineHeight:64
+    },
+  
+  };
+  
+  
+  export default text_styles;
+  
---
+++ Styles/TypoSkin.js
@@ -0,0 +1,84 @@
+const TypoSkin ={
+    
+    Headline1 :{
+        fontSize  :38,
+        lineHeght :48,
+        fontWeght :'Medium',
+    },
+    Headline2 :{
+        fontSize  :30,
+        lineHeght :38,
+        fontWeght :'Medium',
+    },
+    Headline3 :{
+        fontSize  :24,
+        lineHeght :32,
+        fontWeght :'Medium',
+    },
+    Headline4:{
+        fontSize  :20,
+        lineHeght :28,
+        fontWeght :'Medium',
+    },
+    Headline5:{
+        fontSize  :16,
+        lineHeght :24,
+        fontWeght :'Medium',
+    },
+    Headline6 :{
+        fontSize  :14,
+        lineHeght :22,
+        fontWeght :'Medium',
+    },
+    Large_title :{
+        fontSize  :38,
+        lineHeght :48,
+        fontWeght :'Medium',
+    },
+    title1 :{
+        fontSize  :24,
+        lineHeght :32,
+        fontWeght :'Medium',
+    },
+    title2 :{
+        fontSize  :20,
+        lineHeght :28,
+        fontWeght :'Medium',
+    },
+    title3 :{
+        fontSize  :16,
+        lineHeght :24,
+        fontWeght :'Medium',
+    },
+    title4 :{
+        fontSize  :16,
+        lineHeght :24,
+        fontWeght :'Regular',
+    },
+    title5 :{
+        fontSize  :14,
+        lineHeght :22,
+        fontWeght :'Medium',
+    },
+    title6 :{
+        fontSize  :14,
+        lineHeght :22,
+        fontWeght :'Regular',
+    },
+    ButtonText1:{
+        fontSize  :16,
+        lineHeght :24,
+        fontWeght :'Regular',
+    },
+    ButtonText2:{
+        fontSize  :14,
+        lineHeght :22,
+        fontWeght :'Regular',
+    },
+    ButtonText3 :{
+        fontSize  :12,
+        lineHeght :16,
+        fontWeght :'Regular',
+    },
+      
+}
