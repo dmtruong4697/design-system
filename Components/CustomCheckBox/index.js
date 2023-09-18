7e5ddb14854a8790f30431d30f66b86a6f9d7693
dqphuong2k2 <dqphuong2k2@gmail.com>
2023-09-18 10:42:46 +0700

    add custom inputfield

---
+++ Components/CustomCheckBox/index.js
@@ -0,0 +1,133 @@
+import React, {useEffect, useRef} from 'react';
+import {Animated, Text, View} from 'react-native';
+import {AppIcons} from '../../constants/AppResource';
+import PropTypes, {any} from 'prop-types';
+import {Controller} from 'react-hook-form';
+import {AppColors} from '../../constants/AppStyle';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+
+CustomCheckBox.propTypes = {
+  control: PropTypes.any,
+  required: PropTypes.exact({
+    value: PropTypes.bool,
+    message: PropTypes.string,
+  }),
+  name: PropTypes.string,
+  errors: PropTypes.object,
+  alertColor: PropTypes.string,
+  label: PropTypes.string,
+  checkedIconSource: PropTypes.string,
+  fillColor: PropTypes.string,
+  unfillColor: PropTypes.string,
+  iconStyle: PropTypes.object,
+  innerIconStyle: PropTypes.object,
+  textStyle: PropTypes.object,
+};
+
+CustomCheckBox.defaultProps = {
+  control: any,
+  required: {
+    value: false,
+    message: '',
+  },
+  name: '',
+  errors: {},
+  alertColor: AppColors.error,
+  label: 'Label',
+  checkedIconSource: undefined,
+  fillColor: 'red',
+  unfillColor: 'white',
+  iconStyle: {borderColor: 'black'},
+  innerIconStyle: {borderWidth: 2},
+  textStyle: {fontFamily: 'JosefinSans-Regular'},
+};
+
+function CustomCheckBox(props) {
+  const {
+    control,
+    required,
+    name,
+    errors,
+    alertColor,
+    label,
+    checkedIconSource,
+    fillColor,
+    unfillColor,
+    iconStyle,
+    innerIconStyle,
+    textStyle,
+  } = props;
+
+  const animated = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    console.log(errors);
+    if (errors && errors[name]) {
+      Animated.timing(animated, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+      console.log('have error');
+    } else {
+      Animated.timing(animated, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [errors, errors[name]]);
+
+  return (
+    <>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Text style={{flex: 1, marginHorizontal: 10}} numberOfLines={2}>
+          {label}
+        </Text>
+        <Controller
+          control={control}
+          rules={{required: {...required}}}
+          name={name}
+          render={({field: {onChange, onBlur, value}}) => (
+            <BouncyCheckbox
+              size={25}
+              fillColor={fillColor}
+              unfillColor={unfillColor}
+              disableText
+              iconStyle={iconStyle}
+              checkIconImageSource={checkedIconSource}
+              innerIconStyle={innerIconStyle}
+              textStyle={textStyle}
+              onPress={isChecked => {
+                onChange(isChecked);
+              }}
+            />
+          )}
+        />
+      </View>
+      <Animated.View
+        style={{
+          height: animated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [5, 25],
+          }),
+          opacity: animated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+          }),
+        }}>
+        {errors && errors[name] && (
+          <Text style={{color: alertColor && alertColor}}>
+            {errors[name].message}
+          </Text>
+        )}
+      </Animated.View>
+    </>
+  );
+}
+
+export default CustomCheckBox;
---
+++ Components/FormContainer/index.js
@@ -0,0 +1,108 @@
+import React, {createContext} from 'react';
+import {Controller, useForm} from 'react-hook-form';
+import {Button, Text, TextInput, View} from 'react-native';
+import CustomInputField from '../CustomInputField';
+import PropTypes, {any, oneOfType} from 'prop-types';
+import {InputType} from '../../constants/constants';
+import CustomCheckBox from '../CustomCheckBox';
+import {AppColors} from '../../constants/AppStyle';
+
+FormContainer.propTypes = {
+  onSubmitting: PropTypes.func,
+  fields: PropTypes.arrayOf(PropTypes.object),
+  customSubmitButton: PropTypes.func,
+  onChangeForm: PropTypes.func,
+};
+
+FormContainer.defaultProps = {
+  onSubmitting: () => {},
+  fields: [{}],
+  customSubmitButton: () => {},
+  onChangeForm: () => {},
+};
+
+function FormContainer(props) {
+  const {children, onSubmitting, fields, customSubmitButton, onChangeForm} =
+    props;
+
+  const {
+    control,
+    resetField,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: defaultValueFilter,
+  });
+
+  const textInput = (field, index, errors) => {
+    const {label, name, placeholder, rules} = field;
+    return (
+      <CustomInputField
+        control={control}
+        onReset={resetField}
+        rules={{...rules}}
+        borderWidth={1}
+        borderColor={'black'}
+        name={name}
+        key={index}
+        onChangeText={onChangeForm}
+        label={label}
+        errors={errors}
+        placeholder={placeholder}
+      />
+    );
+  };
+
+  const checkbox = (field, index) => {
+    const {label, name, required} = field;
+    return (
+      <CustomCheckBox
+        control={control}
+        key={index}
+        name={name}
+        errors={errors}
+        label={label}
+        required={required}
+      />
+    );
+  };
+
+  const _renderContent = errors =>
+    fields &&
+    fields.map((field, index) => {
+      switch (field?.type) {
+        case InputType.textInput:
+          return textInput(field, index, errors);
+        case InputType.checkBox:
+          return checkbox(field, index, errors);
+        default:
+          return textInput(field, index, errors);
+      }
+    });
+
+  const defaultValueFilter = {};
+  fields.forEach(
+    field =>
+      (defaultValueFilter[field.name] =
+        field?.type === InputType.checkBox ? false : ''),
+  );
+
+  const onSubmit = data => onSubmitting(data);
+
+  const sayHello = () => {
+    console.log(errors);
+  };
+
+  return (
+    <View>
+      {_renderContent(errors)}
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      {customSubmitButton(sayHello)}
+      {/* {React.cloneElement(children, {
+        sayHello,
+      })} */}
+    </View>
+  );
+}
+
+export default FormContainer;
